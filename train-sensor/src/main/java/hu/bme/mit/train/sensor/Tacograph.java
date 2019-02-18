6cb2aecbf9900cbc26ddf4c37fbcb98f2502aa3c
dori1 <szabo.dori94@gmail.com>
2019-02-18 09:15:20 -0800

    guava

---
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/Tacograph.java
@@ -0,0 +1,22 @@
+package hu.bme.mit.train.sensor;
+
+import com.google.common.collect.HashBasedTable;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.interfaces.TrainSensor;
+
+
+public class Tachograph {
+
+    private Table adatok = HashBasedTable.create();
+
+    public Tachograph(TrainController controller, TrainUser user) {
+        adatok.put(0,0,System.currentTimeMillis());
+        adatok.put(0,1,user.getJoystickPosition());
+        adatok.put(0,2,controller.getReferenceSpeed());
+    }
+
+    public Table getAdatok() {
+        return adatok;
+    }
+}
